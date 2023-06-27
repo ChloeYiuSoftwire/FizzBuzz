ae747ff089e538c1224b70aa342a52d1919444dd
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-27 15:44:27 +0100

    Part 3 done

--- index.js
+++ index.js
@@ -46,4 +46,31 @@ function partTwo() {
   }
 }
 
-partTwo();
+function partThree() {
+  const prompt = require("prompt-sync")({ sigint: true });
+  const maxNum = prompt("Input upper limit: ");
+  let FIZZ = prompt("Number for Fizz: ");
+  let BUZZ = prompt("Number for Buzz: ");
+  let FEZZ = prompt("Number for Fezz: ");
+  for (let num = 1; num <= maxNum; num++) {
+    let output = "";
+    if (num % 11 === 0) {
+      output = num % 13 === 0 ? "BongFezz" : "Bong";
+    } else if (num % 17 === 0) {
+      output += num % 7 === 0 ? "Bang" : "";
+      output += num % BUZZ === 0 ? "Buzz" : "";
+      output += num % FEZZ === 0 ? "Fezz" : "";
+      output += num % FIZZ === 0 ? "Fizz" : "";
+    } else {
+      output += num % FIZZ === 0 ? "Fizz" : "";
+      output += num % FEZZ === 0 ? "Fezz" : "";
+      output += num % BUZZ === 0 ? "Buzz" : "";
+      output += num % 7 === 0 ? "Bang" : "";
+    }
+    output += output === "" ? num : "";
+
+    console.log(output);
+  }
+}
+
+partThree();
